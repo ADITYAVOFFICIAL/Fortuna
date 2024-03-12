f2ebc9154de4bf32d8159e2c4fc5b5c9156dccc1
ADITYA VERMA <adityav1304@gmail.com>
2024-03-13 00:55:03 +0530

    a

--- CONTRACTS/GiftBox.sol
+++ CONTRACTS/GiftBox.sol
@@ -1,10 +1,11 @@
-pragma solidity ^0.8.20;
- 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
- 
-contract RedEnvelope {
- 
-    struct Envelope {
+
+contract GiftBox {
+
+    struct Box {
         uint256 amount;
         uint256 creationTime;
         uint256 timeLimit;
@@ -12,24 +13,24 @@ contract RedEnvelope {
         address token;
         mapping(address => bool) claimed;
     }
- 
-    event EnvelopeCreated(uint256 id);
- 
-    mapping(uint256 => Envelope) public envelopes;
+
+    event BoxCreated(uint256 id);
+
+    mapping(uint256 => Box) public boxes;
     mapping(address => uint256[]) public history;
- 
-    function createEnvelope(uint256 _timeLimitInSeconds, address _token) public payable returns (uint256) {
+
+    function createBox(uint256 _timeLimitInSeconds, address _token) public payable returns (uint256) {
         require(msg.value > 0, "Amount should be greater than 0");
- 
+
         uint256 id = uint256(keccak256(abi.encodePacked(block.timestamp, msg.sender))) % (10**18);
- 
-        envelopes[id].amount = msg.value;
-        envelopes[id].creationTime = block.timestamp;
-        envelopes[id].timeLimit = _timeLimitInSeconds;
-        envelopes[id].creator = msg.sender;
-        envelopes[id].token = _token;
- 
-        emit EnvelopeCreated(id);
+
+        boxes[id].amount = msg.value;
+        boxes[id].creationTime = block.timestamp;
+        boxes[id].timeLimit = _timeLimitInSeconds;
+        boxes[id].creator = msg.sender;
+        boxes[id].token = _token;
+
+        emit BoxCreated(id);
 
         history[msg.sender].push(id);
         return id;
@@ -37,18 +38,18 @@ contract RedEnvelope {
 
     function claim(uint256 _id) public returns (uint256) {
         require(!isInvalidID(_id), "Invalid ID");
-        require(getTimeLeft(_id) > 0, "Time limit exceeded");
-        require(envelopes[_id].amount > 0, "No funds available");
-        require(!envelopes[_id].claimed[msg.sender], "Already claimed");
- 
-        if (envelopes[_id].token != address(0)) {
-            require(IERC721(envelopes[_id].token).balanceOf(msg.sender) > 0, "Must own the specified token to claim");
+        require(getTimeLeft(_id) > 0, "Time limit exceeded [Expired]");
+        require(boxes[_id].amount > 0, "No funds available");
+        require(!boxes[_id].claimed[msg.sender], "Already claimed Gift Box");
+
+        if (boxes[_id].token != address(0)) {
+            require(IERC721(boxes[_id].token).balanceOf(msg.sender) > 0, "Must own the specified token to claim");
         }
- 
-        uint256 claimAmount = getRandomAmount(envelopes[_id].amount);
-        envelopes[_id].amount -= claimAmount;
-        envelopes[_id].claimed[msg.sender] = true;
- 
+
+        uint256 claimAmount = getRandomAmount(boxes[_id].amount);
+        boxes[_id].amount -= claimAmount;
+        boxes[_id].claimed[msg.sender] = true;
+
         payable(msg.sender).transfer(claimAmount);
         return claimAmount;
     }
@@ -56,33 +57,33 @@ contract RedEnvelope {
     function addToEnvelope(uint256 _id) public payable {
         require(!isInvalidID(_id), "Invalid ID");
         require(msg.value > 0, "Amount should be greater than 0");
-        require(getTimeLeft(_id) > 0, "Envelope has expired");
+        require(getTimeLeft(_id) > 0, "Gift Box has expired");
 
-        envelopes[_id].amount += msg.value;
+        boxes[_id].amount += msg.value;
     }
- 
+
     function reclaim(uint256 _id) public {
         require(!isInvalidID(_id), "Invalid ID");
-        require(msg.sender == envelopes[_id].creator, "Only the creator can reclaim");
-        require(block.timestamp > envelopes[_id].creationTime + envelopes[_id].timeLimit, "Cannot reclaim before time limit");
- 
-        uint256 remainingAmount = envelopes[_id].amount;
-        envelopes[_id].amount = 0;
+        require(msg.sender == boxes[_id].creator, "Only the creator can reclaim");
+        require(block.timestamp > boxes[_id].creationTime + boxes[_id].timeLimit, "Cannot reclaim before time limit");
+
+        uint256 remainingAmount = boxes[_id].amount;
+        boxes[_id].amount = 0;
         (bool sent, bytes memory data) = payable(msg.sender).call{value: remainingAmount}("");
         require(sent, "Failed to send Ether");
-        return remainingAmount;
-    } 
- 
+    }
+
     function getRandomAmount(uint256 _max) private view returns (uint256) {
+        require(_max > 0, "Max must be greater than 0");
         return uint256(keccak256(abi.encodePacked(block.timestamp))) % _max;
     }
 
     function getTimeLeft(uint256 _id) public view returns (uint256) {
         require(!isInvalidID(_id), "Invalid ID");
         uint256 currentTime = block.timestamp;
-        uint256 creationTime = envelopes[_id].creationTime;
-        uint256 timeLimit = envelopes[_id].timeLimit;
-        
+        uint256 creationTime = boxes[_id].creationTime;
+        uint256 timeLimit = boxes[_id].timeLimit;
+
         if (currentTime > creationTime + timeLimit) {
             return 0;
         } else {
@@ -91,12 +92,12 @@ contract RedEnvelope {
     }
 
     function isInvalidID(uint256 _id) public view returns (bool) {
-        return envelopes[_id].creator == address(0);
+        return boxes[_id].creator == address(0);
     }
 
     function getRemainingAmount(uint256 _id) public view returns (uint256) {
         require(!isInvalidID(_id), "Invalid ID");
-        return envelopes[_id].amount;
+        return boxes[_id].amount;
     }
 
     function getHistory() public view returns (uint256[] memory) {
--- src/pages/landing.js
+++ src/pages/landing.js
@@ -5,6 +5,8 @@ import batGif from '../assets/bat.gif';
 import 'aos/dist/aos.css';
 import { useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
+import backy from '../assets/bg.gif';
+
 const LandingPage = () => {
     const isMounted = useRef(false);
 
@@ -14,15 +16,16 @@ const LandingPage = () => {
             AOS.init();
         }
     }, [])
+
     return (
-        <>
+        <div className="landing-page-container" style={{ borderRadius: '20px',backgroundImage: `url(${backy})` }}>
             <div className="grid md:grid-cols-2 gap-y-20 md:gap-y-0">
                 <div className="h-full flex gap-8 flex-col justify-center text-[Montserrat] order-2">
-                <div className="">
-    <h1 data-aos="fade-up" className="text-[#ee8650] uppercase text-7xl font-semibold font-[Oswald] leading-normal tracking-narrow">FORTUNA</h1>
-    <h1 data-aos="fade-up" className="glow-text text-[#ee8650] uppercase text-2xl font-semibold font-[Oswald] leading-normal tracking-narrow mt-2">TEAM MAVERICKS</h1>
-    <p data-aos="fade-up" data-aos-delay={100} className="text-[#ee8650] my-6 font-semibold font-[Oswald] tracking-wide">A project inspired by our tradition of gifting & spreading the joys of life , but elevated to the realm of NFTs, developed for the Layers 2.0 hackathon.</p>
-</div>
+                    <div>
+                        <h1 data-aos="fade-up" className="text-[#ee8650] uppercase text-7xl font-semibold font-[Oswald] leading-normal tracking-narrow">FORTUNA</h1>
+                        <h1 data-aos="fade-up" className="glow-text text-[#ee8650] uppercase text-2xl font-semibold font-[Oswald] leading-normal tracking-narrow mt-2">TEAM MAVERICKS</h1>
+                        <p data-aos="fade-up" data-aos-delay={100} className="text-[#ee8650] my-6 font-semibold font-[Oswald] tracking-wide">A project inspired by our tradition of gifting & spreading the joys of life , but elevated to the realm of NFTs, developed for the Layers 2.0 hackathon.</p>
+                    </div>
                     <div>
                         <Link to={'/create-link'}>
                             <button data-aos-delay={200} data-aos="fade-up" className="bg-[#ee8650] font-[Oswald] tracking-wide rounded-md flex gap-2 px-3 py-2 items-center font-semibold">Create Your's Giftbox Now <AiOutlineArrowRight className="text-xl" /></button>
@@ -31,27 +34,27 @@ const LandingPage = () => {
                     <div className="my-20 md:my-0">&nbsp;</div>
                 </div>
                 <div className="flex items-center justify-center order-1 md:order-2">
-      <img
-        data-aos="fade-left"
-        data-aos-delay={100}
-        src={heroImg}
-        className="md:w-[60%] w-[75%] h-auto rotating-image"
-        alt="Hero Image"
-      />
-    </div>
+                    <img
+                        data-aos="fade-left"
+                        data-aos-delay={100}
+                        src={heroImg}
+                        className="md:w-[60%] w-[75%] h-auto rotating-image"
+                        alt="Hero Image"
+                    />
+                </div>
             </div>
-            <img 
-  src={batGif} 
-  alt="Bat" 
-  className="absolute bottom-0 left-0 w-20 h-auto animate-bat" 
-  style={{ 
-    animation: 'moveBat 6s linear infinite',
-    width: 'auto',  // Reset width to 'auto' to maintain the original size
-    height: '120px'  // Set height to maintain aspect ratio
-  }} 
-/>
-        </>
-    )
+            <img
+                src={batGif}
+                alt="Bat"
+                className="absolute bottom-0 left-0 w-20 h-auto animate-bat"
+                style={{
+                    animation: 'moveBat 6s linear infinite',
+                    width: 'auto',
+                    height: '120px'
+                }}
+            />
+        </div>
+    );
 }
 
 export default LandingPage;
