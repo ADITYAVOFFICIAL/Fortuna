7e96b7b2f82d7d6d92fdb711a61d161fb784ced3
ADITYA VERMA <adityav1304@gmail.com>
2024-03-15 02:16:33 +0530

    Update topnfts.js

--- src/pages/topnfts.js
+++ src/pages/topnfts.js
@@ -10,16 +10,25 @@ const TopNFT = () => {
     useEffect(() => {
         const fetchTopNFTs = async () => {
             try {
-                const response = await axios.get('https://api.opensea.io/api/v2/collection/chikn/nfts?limit=50', {
+                const response = await axios.get('https://api.opensea.io/api/v2/collection/lilpudgys/nfts?limit=100', {
                     headers: {
                         'X-API-KEY': 'e238fc3516a847fe934466edd688f83c'
                     }
                 });
-                const topNFTsData = response.data.nfts;
+                const topNFTsData = response.data.nfts.map(nft => ({
+                    ...nft,
+                    formattedUpdatedAt: new Date(nft.updated_at).toLocaleString('en-US', {
+                        day: 'numeric',
+                        month: 'short',
+                        year: 'numeric',
+                        hour: 'numeric',
+                        minute: 'numeric',
+                        hour12: true
+                    })
+                }));
                 setTopNFTs(topNFTsData);
-                console.log('Top NFTs:', topNFTsData);
             } catch (error) {
-                console.error('Error fetching top NFTs:', error);
+                // Handle error if needed
             }
         };
 
@@ -41,10 +50,10 @@ const TopNFT = () => {
                     <div key={nft.identifier} className="nft-item glowy">
                         <img src={nft.image_url} alt={nft.name || "NFT"} />
                         <div className="nft-details">
-                            <p className='font-[Oswald] tracking-wider'  style={{ color: "white" }}>Name: {nft.name || "Unnamed NFT"}</p>
+                            <p className='font-[Oswald] tracking-wider'  style={{ color: "#ee8650" }}>Name: {nft.name || "Unnamed NFT"}</p>
                             <p className='font-[Oswald] tracking-wider' style={{ color: "white" , wordBreak: "break-word" }}>Contract: {nft.contract}</p>
                             <p  className='font-[Oswald] tracking-wider' style={{ color: "white" }}>Token Standard: {nft.token_standard}</p>
-                            <p className='font-[Oswald] tracking-wider'  style={{ color: "white" }}>Type: Avalanche Fuji</p>
+                            <p className='font-[Oswald] tracking-wider' style={{ color: "white" , wordBreak: "break-word" }}>Last Updated : {nft.formattedUpdatedAt}</p>
                             {/* Add more details as needed */}
                         </div>
                     </div>
