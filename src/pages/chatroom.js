9471aae0908a705fedc588e4dcfea3c43ba8c9f4
ADITYA VERMA <adityav1304@gmail.com>
2024-03-15 01:56:15 +0530

    a

--- src/pages/chatroom.js
+++ src/pages/chatroom.js
@@ -1,13 +1,28 @@
-import { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const ChatRoom = () => {
   const [messages, setMessages] = useState([]);
   const [inputMessage, setInputMessage] = useState('');
+  const [walletAddress, setWalletAddress] = useState('');
+
+  useEffect(() => {
+    async function getWalletAddress() {
+      if (window.ethereum) {
+        // Request access to MetaMask accounts
+        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+        if (accounts.length > 0) {
+          setWalletAddress(accounts[0]);
+        }
+      }
+    }
+
+    getWalletAddress();
+  }, []);
 
   const sendMessage = () => {
     if (inputMessage.trim() !== '') {
       const newMessage = {
-        sender: 'User',
+        sender: walletAddress !== '' ? walletAddress : 'User',
         content: inputMessage.trim(),
       };
       setMessages([...messages, newMessage]);
@@ -15,25 +30,31 @@ const ChatRoom = () => {
     }
   };
 
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      sendMessage();
+    }
+  };
+
   return (
     <div>
-        
-      <div className="messages-container" style={{ backgroundColor: "#333", padding: "10px", marginBottom: "10px" ,borderRadius:"8px"}}>
-      <h1 data-aos-delay={500} data-aos="fade-up" className="text-white font-semibold flex rowdies-regular items-center text-xl gap-1 uppercase glow-text1 mb-10">YOUR ANONYMOUS CHATS HERE</h1>
+      <div className="messages-container" style={{ backgroundColor: "#333", padding: "10px", marginBottom: "10px", borderRadius: "8px" }}>
+        <h1 data-aos-delay={500} data-aos="fade-up" className="text-white font-semibold flex rowdies-regular items-center text-xl gap-1 uppercase glow-text1 mb-10">YOUR ANONYMOUS CHATS HERE</h1>
         {messages.map((message, index) => (
-          <div key={index} className="message font-[Oswald] tracking-widest" style={{ color: "white", marginBottom: "5px" }}>
-            <span className="sender">{message.sender}:</span> {message.content}
+          <div key={index} className="message font-[Oswald] tracking-widest" style={{ color: "white", marginBottom: "15px", marginRight: "15px" }}>
+            <span style={{ marginRight: "2px" }} className="sender">{message.sender} :</span> {message.content}
           </div>
         ))}
-
       </div>
-      <div className="input-container" style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+      <div className="input-container black-container" style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
         <input
           type="text"
+          className='font-[Oswald]'
           value={inputMessage}
           onChange={(e) => setInputMessage(e.target.value)}
+          onKeyPress={handleKeyPress} // Call handleKeyPress function on key press
           placeholder="Type your message..."
-          style={{ padding: "5px", marginRight: "5px" ,borderRadius:"8px"}}
+          style={{ padding: "10px", marginRight: "15px", borderRadius: "8px", width: "500px" }}
         />
         <button className='glowy font-[Oswald] tracking-wider' style={{ color: "black", backgroundColor: "#ee8650", border: "none", padding: "5px 10px", borderRadius: "5px" }} onClick={sendMessage}>Send</button>
       </div>
